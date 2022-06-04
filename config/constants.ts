587ff575b17d2a34ff4912aa3da47ae341bf5dfe
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2022-06-04 09:14:31 +0300

    Update constants.ts

--- config/constants.ts
+++ config/constants.ts
@@ -3,5 +3,5 @@ export const SOCIAL_LINK_TWITTER = 'https://twitter.com/winzeland';
 export const SOCIAL_LINK_INSTAGRAM = 'https://www.instagram.com/playwinzeland';
 export const SOCIAL_LINK_GITHUB = 'https://www.github.com/winzeland';
 export const SOCIAL_LINK_FACEBOOK = 'https://www.facebook.com/winzeland';
-export const SOCIAL_LINK_TELEGRAM = 'https://t.me/winzelandgame';
+export const SOCIAL_LINK_TELEGRAM = 'https://t.me/winzeland';
 export const SOCIAL_LINK_REDDIT = 'https://www.reddit.com/r/winzeland';
