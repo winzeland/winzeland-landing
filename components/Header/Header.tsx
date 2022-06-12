ed7efcb31beda45ee9918a43a9765021c2b6578a
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2022-06-12 12:07:35 +0300

    fix: header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,12 +2,18 @@ import { NavLink } from './NavLink';
 import OpenWindowIcon from 'assets/icons/open-in-window.svg';
 import Image from 'next/image';
 import Script from 'next/script';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import styles from './NavLink.module.css';
+import classNames from 'classnames';
 
 export const Header = () => {
   useEffect(() => {
     prefinery('recordFormImpression');
   }, []);
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = useCallback(() => setMenuOpen(value => !value), []);
+
   return (
     <>
       <Script
@@ -24,53 +30,67 @@ export const Header = () => {
       <header className="w-full fixed top-0 z-50 pt-5 lg:pt-8">
         <div className="container relative flex h-full items-center justify-end mx-auto">
           <div className="items-center flex">
-            <ul className="flex space-x-2 mr-2">
-              <li>
-                <NavLink href="/" title="Home" />
-              </li>
-              <li>
-                <NavLink href="/join" title="Join" />
-              </li>
-              <li>
-                <NavLink href="/paper" title="Paper" />
-              </li>
-              <li>
-                <NavLink
-                  href="https://blog.winzeland.com"
-                  title={
-                    <>
-                      Blog&nbsp;
-                      <Image
-                        src={OpenWindowIcon}
-                        alt="Blog"
-                        width={14}
-                        height={14}
-                        priority
-                      />
-                    </>
-                  }
-                  target="_blank"
-                />
-              </li>
-              <li>
-                <NavLink
-                  href="https://opensea.io/collection/winzeland-winzer"
-                  title={
-                    <>
-                      OpenSea&nbsp;
-                      <Image
-                        src={OpenWindowIcon}
-                        alt="Open Sea"
-                        width={14}
-                        height={14}
-                        priority
-                      />
-                    </>
-                  }
-                  target="_blank"
-                />
-              </li>
-            </ul>
+            <div>
+              <div className="mb-3 flex justify-end md:hidden">
+                <button className={styles.link} onClick={toggleMenu}>
+                  Menu
+                </button>
+              </div>
+              <ul
+                className={classNames(
+                  'flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2 md:mr-2',
+                  {
+                    'hidden md:flex': !menuOpen,
+                  },
+                )}
+              >
+                <li>
+                  <NavLink href="/" title="Home" />
+                </li>
+                <li>
+                  <NavLink href="/join" title="Join" />
+                </li>
+                <li>
+                  <NavLink href="/paper" title="Paper" />
+                </li>
+                <li>
+                  <NavLink
+                    href="https://blog.winzeland.com"
+                    title={
+                      <>
+                        Blog&nbsp;
+                        <Image
+                          src={OpenWindowIcon}
+                          alt="Blog"
+                          width={14}
+                          height={14}
+                          priority
+                        />
+                      </>
+                    }
+                    target="_blank"
+                  />
+                </li>
+                <li>
+                  <NavLink
+                    href="https://opensea.io/collection/winzeland-winzer"
+                    title={
+                      <>
+                        OpenSea&nbsp;
+                        <Image
+                          src={OpenWindowIcon}
+                          alt="Open Sea"
+                          width={14}
+                          height={14}
+                          priority
+                        />
+                      </>
+                    }
+                    target="_blank"
+                  />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </header>
